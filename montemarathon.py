fe69e721c3260db3d0cb8bfdb972f8e32f9ae596
Andrew Garcia <garcia.gtr@gmail.com>
2019-03-30 17:59:03 -0400

    improved code; shows 1st 2nd and 3rd places for run jog run-jog marathon strategies

--- montemarathon.py
+++ montemarathon.py
@@ -15,15 +15,16 @@ marathon race
 Andrew Garcia, 2019'''
 
 
-'bad runners'
-probs_run = [0.40, 0.40, 0.01]
-'better runners'
-#probs_run = [0.20, 0.30, 0.01]
-'joggers (all the same)'
-probs_jog = [0.01, 0.00001, 0.00001]
+'bad runners - probabilities for injuries / exhaustion high '
+#probs_run = [0.40, 0.40, 0.01]
+#v_run = 12.5        #speed, miles per hour
 
-
+'better runners - probabilities for injuries / exhaustion reduced'
+probs_run = [0.20, 0.30, 0.01]
 v_run = 12.5        #speed, miles per hour
+
+'joggers probabilities all the same whether good or bad'
+probs_jog = [0.01, 0.00001, 0.00001]
 v_jog = 6        #speed, miles per hour
 
 
@@ -60,8 +61,11 @@ def out(k,t,probs,speed,blisters):
     t += 1          #1 minute passes
     
     return k,t,blisters
-    
 
+
+'MONTE CARLO ALGORITHMS FOR RACE CONTESTANTS'
+
+'pure runners - ran entire race'
 def runner(L):
         
     'clock starts @ time t = 0 min and distance k = 0 miles'    
@@ -71,7 +75,7 @@ def runner(L):
         k,t,blisters = out(k,t,probs_run,v_run,blisters)
     return k,t
     
-    
+'pure joggers - jogged entire race'
 def jogger(L):
     
     'clock starts @ time t = 0 min and distance k = 0 miles'    
@@ -81,7 +85,7 @@ def jogger(L):
         k,t,blisters = out(k,t,probs_jog,v_jog,blisters)
     return k,t
     
-'running-jogging intervals'
+'runner-joggers - alternated between running and jogging'
 def rj_int(L):
     
     'lower probability of hitting the wall if alternating:'
@@ -96,8 +100,9 @@ def rj_int(L):
         
     return k,t
     
-
-def race_winner(L):
+'--------------------------------------------------------------------------'
+'1st 2nd or 3rd place'
+def race_places(L):
 
     dr,tr = runner(L)
     dj,tj = jogger(L)
@@ -105,47 +110,120 @@ def race_winner(L):
     drj,trj = rj_int(L)
     
     times = [tr,tj,trj]
-    whowon = np.zeros(3)
-    winner = times.index(np.min(times))
-    whowon[winner] =1
+    order = list(np.sort(times))
+    
+    places = [order.index(times[0]),order.index(times[1]),order.index(times[2])]
 
-    return whowon
+    return places
 
 
+def assign_places(L,rers1st, jers1st, rjers1st,rers2nd, jers2nd, \
+                  rjers2nd,rers3rd, jers3rd, rjers3rd):
+
+    run,jog,runjog=race_places(L)
+
+    rers1st.append(1 if run == 0 else 0)
+    jers1st.append(1 if jog == 0 else 0)
+    rjers1st.append(1 if runjog == 0 else 0)
+    
+    rers2nd.append(1 if run == 1 else 0)
+    jers2nd.append(1 if jog == 1 else 0)
+    rjers2nd.append(1 if runjog == 1 else 0)
+    
+    rers3rd.append(1 if run == 2 else 0)
+    jers3rd.append(1 if jog == 2 else 0)
+    rjers3rd.append(1 if runjog == 2 else 0)
+
+    R1st, J1st, RJ1st = np.sum(rers1st),np.sum(jers1st),np.sum(rjers1st)
+    R2nd, J2nd, RJ2nd = np.sum(rers2nd),np.sum(jers2nd),np.sum(rjers2nd)
+    R3rd, J3rd, RJ3rd = np.sum(rers3rd),np.sum(jers3rd),np.sum(rjers3rd)
+    
+    return R1st, J1st, RJ1st, R2nd, J2nd, RJ2nd, R3rd, J3rd, RJ3rd 
+
+'ITERATIONS'
+
+def fast_iters(N):
+    L = 26.2    #miles
+
+    j = 0
+    rers1st, jers1st, rjers1st = [],[],[]
+    rers2nd, jers2nd, rjers2nd = [],[],[]
+    rers3rd, jers3rd, rjers3rd = [],[],[]
+
+    while j < N:
+        R1st, J1st, RJ1st, R2nd, J2nd, RJ2nd, R3rd, J3rd, RJ3rd = \
+        assign_places(L,rers1st, jers1st, rjers1st, \
+                      rers2nd, jers2nd, rjers2nd, rers3rd, jers3rd, rjers3rd)
+        j+=1
+    
+    print('Results {} Marathon races'.format(j))
+    print('Runners: \n1st place {0}({3}%)\n2nd place {1}({4}%) \
+          \n3rd place {2}({5}%)'.\
+          format(R1st,R2nd,R3rd,R1st*100/j,R2nd*100/j,R3rd*100/j))
+    
+    print('\nJoggers: \n1st place {0}({3}%)\n2nd place {1}({4}%) \
+          \n3rd place {2}({5}%)'.\
+          format(J1st,J2nd,J3rd,J1st*100/j,J2nd*100/j,J3rd*100/j))
+    
+    print('\nRunner-Joggers: \n1st place {0}({3}%)\n2nd place {1}({4}%) \
+          \n3rd place {2}({5}%)'.\
+          format(RJ1st,RJ2nd,RJ3rd,RJ1st*100/j,RJ2nd*100/j,RJ3rd*100/j))
+
+print(fast_iters(1000))
+
+        
 def dyn_bar(N):
     L = 26.2    #miles
 
     j = 0
-    rers, jers, rjers = [],[],[]
+    rers1st, jers1st, rjers1st = [],[],[]
+    rers2nd, jers2nd, rjers2nd = [],[],[]
+    rers3rd, jers3rd, rjers3rd = [],[],[]
+
     while j < N:
 
         plt.clf()
-        run,jog,runjog=race_winner(L)
         
-        x=[-0.125, 0.4, 1- 0.125]
-
+        R1st, J1st, RJ1st, R2nd, J2nd, RJ2nd, R3rd, J3rd, RJ3rd = \
+        assign_places(L,rers1st, jers1st, rjers1st, \
+                      rers2nd, jers2nd, rjers2nd, rers3rd, jers3rd, rjers3rd)
         
-        rers.append(run)
-        jers.append(jog)
-        rjers.append(runjog)
-
-        RUNNER, JOGGER, RJ = np.sum(rers),np.sum(jers),np.sum(rjers)
-        y = [RUNNER, JOGGER, RJ]
+        
+        x=[0, 1, 2]
+        y = [R2nd,R1st,R3rd]
+        
+        x2=[4,5,6]
+        y2 = [J2nd,J1st,J3rd]
+#        
+        x3 = [8,9,10]
+        y3 = [RJ2nd,RJ1st,RJ3rd]
     
         j+=1
-#        print(j)
-        plt.text(-0.125,RUNNER,'{} %'.format(np.round(RUNNER*100/j,2)),size=15)
-        plt.text(0.4,JOGGER,'{} %'.format(np.round(JOGGER*100/j,2)),size=15)
-        plt.text(1- 0.125,RJ,'{} %'.format(np.round(RJ*100/j,2)),size=15)
 
-        plt.bar(x,y,0.25)
-#        plt.xlim(-0.5,1.5)
+        plt.bar(x,y,0.8,label='running')
+        plt.bar(x2,y2,0.8,label='jogging')
+        plt.bar(x3,y3,0.8,label='run-jog intervals')
+
         
-        plt.ylim(0,N)
-        plt.suptitle('Marathon winners - strategies',size =13)
+        plt.legend(prop={'size': 13})
+        plt.ylim(0,N*1.3)
+        plt.suptitle('Marathon results',size =13)
         plt.title('{} marathon contests simulated'.format(j),size=13)
-        plt.xticks(x,['running','jogging','run-jog intervals'],size=15)
+        plt.xticks([0,1,2,4,5,6,8,9,10],['2nd','1st','3rd']*3,size=13)
+
+
         plt.pause(0.000001)
         
-dyn_bar(100)
+    plt.text(0-.5,R2nd+1,'{} %'.format(np.round(R2nd*100/j,2)),size=14)
+    plt.text(1-.5,R1st+1,'{} %'.format(np.round(R1st*100/j,2)),size=14)
+    plt.text(2-.5,R3rd+1,'{} %'.format(np.round(R3rd*100/j,2)),size=14)
 
+    plt.text(4-.5,J2nd+1,'{} %'.format(np.round(J2nd*100/j,2)),size=14,color='blue')
+    plt.text(5-.5,J1st+1,'{} %'.format(np.round(J1st*100/j,2)),size=14,color='blue')
+    plt.text(6-.5,J3rd+1,'{} %'.format(np.round(J3rd*100/j,2)),size=14,color='blue')
+    
+    plt.text(8-.5,RJ2nd+1,'{} %'.format(np.round(RJ2nd*100/j,2)),size=14,color='magenta')
+    plt.text(9-.5,RJ1st+1,'{} %'.format(np.round(RJ1st*100/j,2)),size=14,color='magenta')
+    plt.text(10-.5,RJ3rd+1,'{} %'.format(np.round(RJ3rd*100/j,2)),size=14,color='magenta')
+
+dyn_bar(100)
